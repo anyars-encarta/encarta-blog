beee285fde0b906bc8fcba46a07a839b4e60b6cc
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-14 19:11:11 +0000

    Create and run the necessary migration files

---
+++ db/migrate/20231114183027_create_tables.rb
@@ -0,0 +1,33 @@
+class CreateTables < ActiveRecord::Migration[7.0]
+    def change
+      create_table :users do |t|
+        t.string :name
+        t.string :photo
+        t.text :bio
+        t.integer :posts_counter
+        t.timestamps
+      end
+  
+      create_table :posts do |t|
+        t.belongs_to :user, foreign_key: { to_table: :users, name: "author_id" }, index: true
+        t.string :title
+        t.text :text
+        t.integer :comments_counter
+        t.integer :likes_counter
+        t.timestamps
+      end
+  
+      create_table :comments do |t|
+        t.belongs_to :user, foreign_key: true
+        t.belongs_to :post, foreign_key: true
+        t.text :text
+        t.timestamps
+      end
+  
+      create_table :likes do |t|
+        t.belongs_to :user, foreign_key: true
+        t.belongs_to :post, foreign_key: true
+        t.timestamps
+      end
+    end
+  end
