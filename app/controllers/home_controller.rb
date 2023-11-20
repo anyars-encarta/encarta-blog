0f916dd41b637e2343678908340675a6076e3e32
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-20 21:01:07 +0000

    Correct Rubocop errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
-    def index
-      # Action to handle the base URL (https://)
-      @welcome_message = "Welcome to Encarta Blog App!"
-    end
+  def index
+    # Action to handle the base URL (https://)
+    @welcome_message = 'Welcome to Encarta Blog App!'
   end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    def show
-      # Action to handle https://users/745
-      @user = User.find(params[:user_id])
-    end
+  def show
+    # Action to handle https://users/745
+    @user = User.find(params[:user_id])
   end
+end
