04bfa5607c3bd7c62b1373ea28ee398d70a7974a
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-21 07:08:45 +0000

    Add #index action to user controller, view and routes as per review requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
   def show
     # Action to handle https://users/745
     @user = User.find(params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # Individual routes for users, their posts, and specific posts
+  get 'users', to: 'users#index', as: 'users'
   get 'users/:user_id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
