64cf144585f241870f2e29ed95b8090843e02379
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-21 07:22:28 +0000

    Correct rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
+  def index; end
+
   def show
     # Action to handle https://users/745
     @user = User.find(params[:user_id])
