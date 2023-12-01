c30995bbad266e457f31f4f321320cbf10754a8c
anyars-encarta <anyarsencarta@gmail.com>
2023-12-01 17:28:04 +0000

    Complete devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   # Remove the email validation for the name field
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_create :most_recent_posts
+  # after_create :most_recent_posts
 
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
@@ -16,13 +16,13 @@ class User < ApplicationRecord
 
   before_validation :set_default_posts_counter
 
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+  
   private
 
   def set_default_posts_counter
     self.posts_counter ||= 0
-  end
-  
-  def most_recent_posts
-    posts.order(created_at: :desc).limit(3)
   end
 end
