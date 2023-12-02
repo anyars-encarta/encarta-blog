f7d2f5cd505f4515a7a3a7e44202fcd563cda1f3
anyars-encarta <anyarsencarta@gmail.com>
2023-12-02 08:49:32 +0000

    Remove commented code as per CR requested changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :set_current_user
-
-  # attr_reader :current_user
-
-  # private
-
-  # def set_current_user
-  #   @current_user = User.first
-  # end
-
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
